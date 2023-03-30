40b227fa18fd629e54f15635afb6f5aa9b923b69
Riaz <riaz85in@yahoo.co.in>
2023-03-30 22:55:46 +0100

    Modified Webpack config and added polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,20 @@
-const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const createExpoWebpackConfigAsync = require("@expo/webpack-config");
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
-  // Customize the config before returning it.
+  // If you want to add a new alias to the config.
+  config.resolve.alias["moduleA"] = "moduleB";
+
+  // Maybe you want to turn off compression in dev mode.
+  if (config.mode === "development") {
+    config.devServer.compress = false;
+  }
+
+  // Or prevent minimizing the bundle when you build.
+  if (config.mode === "production") {
+    config.optimization.minimize = false;
+  }
+
+  // Finally return the new config for the CLI to use.
   return config;
 };
